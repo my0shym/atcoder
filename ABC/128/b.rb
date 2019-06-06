67b48eddc5ea63d42dce2390a7c1caa85a720245
my0shym <benzenetarou@gmail.com>
2019-06-07 01:51:39 +0900

    add review

--- ABC/128/b.rb
+++ ABC/128/b.rb
@@ -52,3 +52,24 @@ city.each do |c|
     res.push(c.split('_')[1].to_i)
 end
 puts res
+
+
+
+
+
+####################
+# 復習
+####################
+n = gets.to_i
+
+t = []
+for i in 1..n
+    s, p = gets.split
+    p = p.to_i
+    t.push([s, -p, i])
+end
+t.sort_by!{|x| [x[0], x[1]]}
+
+t.each do |i|
+    puts i[2]
+end
